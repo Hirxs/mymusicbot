f4b88deb7e0aa7a24b2a5003bbee042aac07b43f
Hirxs <101616510+Hirxs@users.noreply.github.com>
2022-03-21 20:41:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-        TOKEN: 'OTMzMDE2OTU3MDE0Mzg4NzU2.YebZwQ.A9Rtzk3TNAp0IPL3D7A_TU0KXVw', //write your discord bot token
+        TOKEN: 'OTMzMDE2OTU3MDE0Mzg4NzU2.YebZwQ.ZToATyHfb9OUOWbQxLYK-dXJQW0', //write your discord bot token
         px: 'sh!',
         playing: 'Testing Ayaka Bot v3.0',
 
     opt: {
         DJ: {
-            enabled: false, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
+            enabled: true, //IF YOU WANT ONLY DJS TO USE IT, set false to true.
             roleName: 'DJ', //WRITE WHAT THE NAME OF THE DJ ROLE WILL BE, THEY CAN USE IT ON YOUR SERVER
             commands: ['back', 'clear', 'filter', 'loop', 'pause', 'resume', 'skip', 'stop', 'volume'] //Please don't touch
         },
